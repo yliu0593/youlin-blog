d4e40ccfc30acf0ee576bc06afb108064349b17f
youlin <yliu0593@gmail.com>
2025-11-25 10:24:26 -0500

    remove vercel output

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_Cf8uM_K2.mjs';
--- .vercel/output/_functions/chunks/consts_D5zLN5WR.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "yona";
-const SITE_DESCRIPTION = "记录视力恢复、写作练习与生活折腾的小站";
-
-export { SITE_TITLE as S, SITE_DESCRIPTION as a };
